f96ef41ed019cd561678e1fcd130b833511e3205
Samuel <samuellegg2004@icloud.com>
2021-12-13 11:02:07 +0000

    Initial Commit

---
+++ CellularAutomata/CellularAutomataApp.swift
@@ -0,0 +1,17 @@
+//
+//  CellularAutomataApp.swift
+//  CellularAutomata
+//
+//  Created by Samuel Legg on 13/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct CellularAutomataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
